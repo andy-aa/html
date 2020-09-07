8f72294044a32bb667604096efbb037cefe40af2
andy-aa <46691193+andy-aa@users.noreply.github.com>
2020-09-07 10:50:02 +0300

    Update table8.php

--- examples/table8.php
+++ examples/table8.php
@@ -28,7 +28,6 @@ $table
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
-
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
