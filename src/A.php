16206cf926e17b3e7a786bd2904679cdf105ee68
andy-aa <46691193+andy-aa@users.noreply.github.com>
2020-10-03 02:43:41 +0300

    fix

--- src/A.php
+++ src/A.php
@@ -6,12 +6,4 @@ class A extends AbstractPairedTag
 {
     use HrefTrait;
     use TabIndexTrait;
-
-//    /**
-//     * @return string
-//     */
-//    public function html()
-//    {
-//        return parent::html() . "$this->innerText</$this->tagName>";
-//    }
 }
